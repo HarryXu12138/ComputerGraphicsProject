db2767106b68c08ce33410d4228d3c0e09d4de57
Harry <xuj10@rpi.edu>
2017-11-12 14:41:15 -0500

    Basic visualization done. I will add some more features in the future i.e reopen mp3 file, pause/play, jump to a time...

--- index_files/AudioAnalysis.js
+++ index_files/AudioAnalysis.js
@@ -1,3 +1,18 @@
+var analyserNode;
+
+function getFrequencyData() {
+	var bufferLength = analyserNode.frequencyBinCount;
+	var data = new Uint8Array(bufferLength);
+	analyserNode.getByteFrequencyData(data);
+	return data;
+}
+
+function getTimeDomainData() {
+	var bufferLength = analyserNode.frequencyBinCount;
+	var data = new Uint8Array(bufferLength);
+	analyserNode.getByteTimeDomainData();
+	return data;
+}
 
 function AudioAnalysisInitialize() {
 	var audioContext = window.AudioContext || window.webkitAudioContext;
@@ -5,14 +20,39 @@ function AudioAnalysisInitialize() {
 
 	var audioSource = document.getElementById("AudioSource");
 	audioSource.autoplay = true;
+	audioSource.loop = true;
+
+	document.getElementById("playButton").onclick = function(event) {
+		var audioSource = document.getElementById("AudioSource");
+		if (audioSource.paused) audioSource.play();
+		else audioSource.pause();
+	};
 
 	var sourceNode = audioCtx.createMediaElementSource(audioSource);
 	var gainNode = audioCtx.createGain();
+	analyserNode = audioCtx.createAnalyser();
+	analyserNode.fftSize = 64;
+	analyserNode.minDecibels = -130;
+	analyserNode.maxDecibels = 0;
+	analyserNode.smoothingTimeConstant = 0.8;
 
-	sourceNode.connect(gainNode);
+	sourceNode.connect(analyserNode);
+	analyserNode.connect(gainNode);
 	gainNode.connect(audioCtx.destination);
+}
+
+function showData() {
+	var str = "<table>";
+	var data = getFrequencyData();
+	for (var i = 0; i < data.length; ++i) {
+		str += "<tr><td>" + data[i] + "</td></tr>";
+	}
+	str += "</table>";
+	document.getElementById("DataOutput").innerHTML = str;
+	setTimeout(showData, 50);
 }
 
 window.onload = function() {
 	AudioAnalysisInitialize();
+	showData();
 };
