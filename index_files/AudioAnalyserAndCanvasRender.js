1ba259d6321989f1b87265f5a77610b90477e97c
Harry <xuj10@rpi.edu>
2017-12-13 21:30:29 -0500

    Add time indicator

--- index_files/AudioAnalyserAndCanvasRender.js
+++ index_files/AudioAnalyserAndCanvasRender.js
@@ -59,6 +59,17 @@ function musicControllerInitialize() {
         }
     });
 
+    function timeDraw() {
+        setTimeout(timeDraw, 500);
+        if (player.paused) return;
+        if (formatSeconds(player.currentTime) == "") {
+            document.getElementById("currentTime").innerHTML = "0s";
+        } else {
+            document.getElementById("currentTime").innerHTML = formatSeconds(player.currentTime);
+        }
+        document.getElementById("totalTime").innerHTML = formatSeconds(player.duration);
+    }
+
     function progressDraw() {
         setTimeout(progressDraw, 50);
         if (player.paused) return;
@@ -66,7 +77,44 @@ function musicControllerInitialize() {
         document.getElementById("progress").setAttribute("style", "width: " + progress + "%");
     }
 
+    document.getElementById("currentTime").innerHTML = "0s";
+    document.getElementById("timer").innerHTML = "/";
+    document.getElementById("totalTime").innerHTML = "infinity";
+    timeDraw();
     progressDraw();
+}
+
+function formatSeconds(value) {
+    var theTime = parseInt(value);
+    var theTime1 = 0;
+    var theTime2 = 0;
+    var theTime3 = 0;
+    if(theTime > 60) {
+        theTime1 = parseInt(theTime/60);
+        theTime = parseInt(theTime%60);
+        if(theTime1 > 60) {
+            theTime2 = parseInt(theTime1/60);
+            theTime1 = parseInt(theTime1%60);
+            if(theTime2 > 24){
+                theTime3 = parseInt(theTime2/24);
+                theTime2 = parseInt(theTime2%24);
+            }
+        }
+    }
+    var result = '';
+    if(theTime > 0){
+        result = ""+parseInt(theTime)+"s";
+    }
+    if(theTime1 > 0) {
+        result = ""+parseInt(theTime1)+"m"+result;
+    }
+    if(theTime2 > 0) {
+        result = ""+parseInt(theTime2)+"h"+result;
+    }
+    if(theTime3 > 0) {
+        result = ""+parseInt(theTime3)+"d"+result;
+    }
+    return result;
 }
 
 // Return a Uint8Array represent the current music data in frequency domain
